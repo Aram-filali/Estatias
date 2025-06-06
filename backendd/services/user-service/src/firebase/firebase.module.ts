c34dba0df2e20abc257260e70daff3874e3c5011
ARAM123 <aramfilali25@gmail.com>
2025-06-06 21:42:37 +0100

    iiimz

--- backendd/services/user-service/src/firebase/firebase.module.ts
+++ backendd/services/user-service/src/firebase/firebase.module.ts
@@ -4,13 +4,15 @@ import { FirebaseAuthService } from './firebase-auth.service';
 import { FirebaseClientService } from './firebase-client.service';
 import { FirebasePassService } from './firebase-password';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { User, UserSchema } from '../schema/user.schema'; // ou ajuste le chemin si nécessaire
 
 
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]), // ✅ CORRECTION
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule
   ],
   providers: [FirebaseAdminService, FirebaseClientService, FirebaseAuthService, FirebasePassService],
   exports: [FirebaseAdminService, FirebaseClientService, FirebaseAuthService, FirebasePassService],
