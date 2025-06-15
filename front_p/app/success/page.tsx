34b5cebd72d41a080c3a9e5129d51069261a4c78
ARAM123 <aramfilali25@gmail.com>
2025-06-15 16:43:49 +0100

    updatesss

--- front_p/app/success/page.tsx
+++ front_p/app/success/page.tsx
@@ -1,7 +1,19 @@
 "use client";
+import { Suspense } from 'react';
 import SuccessPage from '../../components/prices/SuccessPage';  // Adjust the import path as needed
+
+// Composant de fallback
+const LoadingFallback = () => (
+  <div style={{ padding: '20px', textAlign: 'center' }}>
+    <p>Loading...</p>
+  </div>
+);
 
 // This is the main page file
 export default function Success() {
-  return <SuccessPage />;
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      <SuccessPage />
+    </Suspense>
+  );
 }
